0302eb6f219fe61b01ff920023ed3d5677e9dc46
Juwan-Hollingsworth <jhollingsworth5@student.clayton.edu>
2024-09-03 22:31:38 -0400

    create api endpoints using express

--- api/app.js
+++ api/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import postRoute from "./routes/post.route.js"
 const app = express();
 const port = 8800
 
@@ -8,6 +8,33 @@ app.use("/api/test", (req,res)=>{
 res.send("It works!")
 
 })
+
+app.use("/api/auth/register", (req,res)=>{
+    res.send("It works!")
+    
+    })
+app.use("/api/auth/login", (req,res)=>{
+    res.send("It works!")
+    
+    })
+app.use("/api/auth/logout", (req,res)=>{
+    res.send("It works!")
+    
+    })
+    
+app.use("/api/posts/", (req,res)=>{
+    res.send("It works!")
+    
+    })
+app.use("/api/posts/", postRoute
+    
+    )
+app.use("/api/posts/12343", (req,res)=>{
+    res.send("It works!")
+    
+    })
+    
+
 
 
 app.listen(port, ()=>{
